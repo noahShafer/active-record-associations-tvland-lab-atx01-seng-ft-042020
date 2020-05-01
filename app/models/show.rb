67dec276c06ee450fc43b01c18202215f18959c2
github username <github email address>
2020-05-01 16:34:23 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,7 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+    def actors_list
+        self.characters.map {|c| Actor.find_by(id: c.actor_id).full_name}
+    end
 end
